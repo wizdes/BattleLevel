d3eeae2958d846455e76ceeacab007bcabbb74e0
Yi Li <yl2326@cornell.edu>
2017-05-21 15:36:29 -0700

    added all the prefabs for the assets provided

---
+++ Assets/Plugins/TestManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class TestManager : MonoBehaviour {
+
+    [MenuItem("My Project/Create Simple Prefab")]
+    static void DoCreateSimplePrefab()
+    {
+        Transform[] transforms = Selection.transforms;
+        foreach (Transform t in transforms)
+        {
+            GameObject prefab = PrefabUtility.CreatePrefab("Assets/Temporary/" + t.gameObject.name + ".prefab", t.gameObject, ReplacePrefabOptions.ReplaceNameBased);
+        }
+    }
+}
