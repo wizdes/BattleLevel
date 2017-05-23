a8456fd3c4b8fdeeda5caed419197f75fa3706a9
Yi Li <yl2326@cornell.edu>
2017-05-22 19:30:00 -0700

    updated the board manager to read the modified JSON

--- Assets/Code/BoardManager.cs
+++ Assets/Code/BoardManager.cs
@@ -1,25 +1,49 @@
 ﻿using System.Collections;
 using System;
+using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BoardManager : MonoBehaviour {
 
     public GameObject[] floorTiles;
-    private int columns = 9;
-    private int rows = 16;
+    private int columns = 16;
+    private int rows = 9;
     private Transform boardHolder;
 
     void BoardSetup(){
         boardHolder = new GameObject("Board").transform;
 
-        for (int x = 0; x < columns; x++){
-            for (int y = 0; y < rows; y++){
-                GameObject toInstantiate = floorTiles[0];
+        // read the numbers from the XML (print?)
+        string[] battleMap = File.ReadAllLines("../SpriteMap/BattleMap1Final.txt");
+        string[] terrainMap = battleMap[0].Split(',');
+        string[] objectMap = battleMap[1].Split(',');
+
+        for (int y = 0; y < rows; y++){
+            for (int x = 0; x < columns; x++){
+
+                // read the number and put it here
+                int terrainValue = int.Parse(terrainMap[x * rows + y]);
+
+                //map the terrainValue to the actual value (should just be an addition/subtraction)
+                int mappedTerrainValue = terrainValue;
+
+                GameObject toInstantiate = floorTiles[mappedTerrainValue];
                 GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
 
                 instance.transform.SetParent(boardHolder);
-            }
+
+				// read the number and put it here
+				int objectValue = int.Parse(objectMap[x * rows + y]);
+
+				//map the terrainValue to the actual value (should just be an addition/subtraction)
+				int mappedObjectValue = objectValue;
+
+                toInstantiate = floorTiles[mappedObjectValue];
+				instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+
+				instance.transform.SetParent(boardHolder);
+			}
         }
     }
 
