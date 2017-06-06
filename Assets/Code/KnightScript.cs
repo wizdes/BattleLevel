1ff9eed905095b4f655b2521255cddd00fd256ea
Yi Li <yl2326@cornell.edu>
2017-06-05 19:46:10 -0700

    added multi-move support
    surprisingly easy.

--- Assets/Code/KnightScript.cs
+++ Assets/Code/KnightScript.cs
@@ -8,6 +8,7 @@ public class KnightScript : MovingObject
     public GameObject mainObject;
     private Animator animator;
     private SpriteRenderer sprite;
+    private bool starto;
 
     public override void Start()
     {
@@ -19,6 +20,16 @@ public class KnightScript : MovingObject
     // Update is called once per frame
     void Update()
     {
+        if (starto == false){
+            List<Tuple<int, int>> movements = new List<Tuple<int, int>>();
+            Tuple<int, int> x = new Tuple<int, int>(0, 1);
+            movements.Add(x);
+			x = new Tuple<int, int>(1, 0);
+			movements.Add(x);
+            Move(movements);
+            starto = true;
+		}
+
         //If it's not the player's turn, exit the function.
         if (this.isMoving)
         {
@@ -85,8 +96,69 @@ public class KnightScript : MovingObject
 
             animator.speed = 3.0f;
 
-            AttemptMove<Component>(horizontal, vertical);
+            Move(horizontal, vertical);
         }
     }
 
+	//Co-routine for moving units from one space to next, takes a parameter end to specify where to move to.
+    protected override IEnumerator SmoothMultipleMovement(List<Tuple<int, int>> movementPoints)
+	{
+		foreach (Tuple<int, int> movementPoint in movementPoints)
+		{
+			int xDir = movementPoint.First;
+			int yDir = movementPoint.Second;
+
+			//Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they encounter one (by attacking it)
+			//Pass in horizontal and vertical as parameters to specify the direction to move Player in.
+			if (xDir != 0)
+			{
+				animator.SetBool("Idle", false);
+				animator.SetBool("WalkBack", false);
+				animator.SetBool("WalkSideways", true);
+
+				if (xDir < 0)
+				{
+					sprite.flipX = true;
+				}
+				else
+				{
+					sprite.flipX = false;
+				}
+			}
+			else if (yDir != 0)
+			{
+				if (yDir > 0)
+				{
+					animator.SetBool("Idle", false);
+					animator.SetBool("WalkSideways", false);
+					animator.SetBool("WalkBack", true);
+				}
+			}
+
+			animator.speed = 3.0f;
+			Vector3 moveAmount = new Vector3(xDir, yDir, 0f);
+			Vector3 end = transform.position + moveAmount;
+
+			//Calculate the remaining distance to move based on the square magnitude of the difference between current position and end parameter. 
+			//Square magnitude is used instead of magnitude because it's computationally cheaper.
+			float sqrRemainingDistance = end.sqrMagnitude;
+
+			//While that distance is greater than a very small amount (Epsilon, almost zero):
+			while (sqrRemainingDistance > float.Epsilon)
+			{
+				Vector3 endPoint = Vector3.MoveTowards(transform.position, end, 0.05f);
+
+				//Call MovePosition on attached Rigidbody2D and move it to the calculated position.
+				transform.Translate(endPoint - transform.position);
+
+				//Recalculate the remaining distance after moving.
+				sqrRemainingDistance = (new Vector3(transform.position.x, transform.position.y) - end).sqrMagnitude;
+
+				//Return and loop until sqrRemainingDistance is close enough to zero to end the function
+				yield return null;
+			}
+		}
+
+		this.isMoving = false;
+	}
 }
--- Assets/Code/MovingObject.cs
+++ Assets/Code/MovingObject.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,12 +19,15 @@ public abstract class MovingObject : MonoBehaviour {
 	}
 
     protected bool Move(int xDir, int yDir){
-        Vector2 start = rb2D.position;
         this.isMoving = true;
-
-        Vector2 end = start + new Vector2(xDir, yDir);
-
         StartCoroutine(SmoothMovement(xDir, yDir));
+
+        return true;
+    }
+
+    protected bool Move(List<Tuple<int, int>> movementPoints){
+        this.isMoving = true;
+        StartCoroutine(SmoothMultipleMovement(movementPoints));
 
         return true;
     }
@@ -55,12 +59,36 @@ public abstract class MovingObject : MonoBehaviour {
         this.isMoving = false;
 	}
 
-    //The virtual keyword means AttemptMove can be overridden by inheriting classes using the override keyword.
-	//AttemptMove takes a generic parameter T to specify the type of component we expect our unit to interact with if blocked (Player for Enemies, Wall for Player).
-	protected virtual void AttemptMove<T>(int xDir, int yDir)
-		where T : Component
+	//Co-routine for moving units from one space to next, takes a parameter end to specify where to move to.
+	protected virtual IEnumerator SmoothMultipleMovement(List<Tuple<int, int>> movementPoints)
 	{
-		//Set canMove to true if Move was successful, false if failed.
-		Move(xDir, yDir);
-	}	
+        foreach(Tuple<int,int> movementPoint in movementPoints){
+            int xDir = movementPoint.First;
+            int yDir = movementPoint.Second;
+
+			Vector3 moveAmount = new Vector3(xDir, yDir, 0f);
+			Vector3 end = transform.position + moveAmount;
+
+			//Calculate the remaining distance to move based on the square magnitude of the difference between current position and end parameter. 
+			//Square magnitude is used instead of magnitude because it's computationally cheaper.
+			float sqrRemainingDistance = end.sqrMagnitude;
+
+			//While that distance is greater than a very small amount (Epsilon, almost zero):
+			while (sqrRemainingDistance > float.Epsilon)
+			{
+				Vector3 endPoint = Vector3.MoveTowards(transform.position, end, 0.05f);
+
+				//Call MovePosition on attached Rigidbody2D and move it to the calculated position.
+				transform.Translate(endPoint - transform.position);
+
+				//Recalculate the remaining distance after moving.
+				sqrRemainingDistance = (new Vector3(transform.position.x, transform.position.y) - end).sqrMagnitude;
+
+				//Return and loop until sqrRemainingDistance is close enough to zero to end the function
+				yield return null;
+			}
+		}
+
+		this.isMoving = false;
+	}
 }
